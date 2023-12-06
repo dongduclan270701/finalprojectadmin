3520b769d69e47ed2863f8595826a892f2ff8ea8
Kass <dongduclan27072001@gmail.com>
2023-12-06 17:18:12 +0700

    commit

--- src/Apis/utils/index.js
+++ src/Apis/utils/index.js
@@ -1,5 +1,5 @@
 // export const API_ROOT = 'http:///localhost:8018'
-export const API_ROOT = 'https://ktech-dashboard-api.onrender.com/'
+export const API_ROOT = 'https://ktech-dashboard-api.onrender.com'
 export const cloudNameProduct = 'dolydpat4';
 export const apiKeyProduct = 'p0kgb66h';
 export const uploadUrlProduct = `https://api.cloudinary.com/v1_1/${cloudNameProduct}/image/upload`;
